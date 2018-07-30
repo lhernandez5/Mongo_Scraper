67f427d79064ac326412cbf8a827c2ddabc7dc98
Luz_Hernandez <hernandezluz18@gmail.com>
2018-07-30 12:23:44 -0400

    deploying to heroku

--- public/app.js
+++ public/app.js
@@ -1,19 +1,139 @@
-$.getJSON("/api/all", function(data) {
-  // Call our function to generate a table body
-  displayResults(data);
+// Grab the articles as a json
+$.getJSON("/api/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the articles information on the page
+    $("#articles").append(
+      "<p data-id='" +
+        data[i]._id +
+        "'>" +
+        data[i].title +
+        "<br />" +
+        data[i].summary +
+        "<br />" +
+        data[i].link +
+        "</p>"
+    );
+  }
 });
 
-function displayResults(scrapedData) {
-  // First, empty the table
-  $("tbody").empty();
+// Whenever someone clicks a p tag
+$(document).on("click", "p", function() {
+  // Empty the notes from the note section
+  $("#comments").empty();
 
-  // Then, for each entry of that json...
-  scrapedData.forEach(function(article) {
-    // Append each of the animal's properties to the table
-    $("tbody").append(
-      "<tr><td>" +article.title + "</td>" +
-        "<td>" + article.link +"</td></tr>"
-    );
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/api/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      for (var i = 0; i < data.comment.length; i++) {
+        $("#comments").append(
+          "<p>" +
+            data.comment[i].title +
+            "<br />" +
+            data.comment[i].body +
+            "<button data-id='" +
+            data._id +
+            "' id='deletecomment'>Delete Comment</button></p>"
+        );
+      }
+
+      var commentP = $("<div data-id='" + data._id + "'></div>");
+      // The title of the article
+      commentP.append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      commentP.append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      commentP.append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      commentP.append(
+        "<button data-id='" +
+          data._id +
+          "' id='savecomment'>Save Comment</button>"
+      );
+      // If there's a note in the article
+      $("#comments").append(commentP);
+
+      if (data.comment) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.comment.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.comment.body);
+      }
+    });
+});
+
+// When you click the savenote button
+$(document).on("click", "#savecomment", function() {
+  event.preventDefault();
+  // Grab the id associated with the article from the submit button
+  var commentP = $(this).parent();
+  var commentC = commentP.children();
+  var commentCC = commentC.children("#comments");
+  var commentText = commentCC.val();
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/api/articles/" + thisId,
+    data: {
+      title: $("#titleinput").val(),
+      // body: $("#bodyinput").val()
+      body: commentText
+    }
+  }).then(function(newComment) {
+    $(".commentText").val("");
+
+    var html =
+      "<p>" +
+      newComment.comment +
+      " - - " +
+      "</p>";
+    $("#" + newComment.thisId + "").append(html);
   });
-}
+  // Empty the notes section
+  // $("#comments").empty();
 
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+$(document).on("click", "#deletecomment", function() {
+  var thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "DELETE",
+    url: "/api/articles/" + thisId
+  }).then(function(err) {
+    if (err) throw err;
+  });
+
+  $.ajax({
+    method: "GET",
+    url: "/api/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log("this is the data " + data);
+      for (var i = 0; i < data.comment.length; i++) {
+        $("#comments").append(
+          "<p>" +
+            data.comment[i].title +
+            "<br />" +
+            data.comment[i].body +
+            "</p><button data-id='" +
+            data._id +
+            "' id='deletecomment'>Delete Comment</button>"
+        );
+      }
+    });
+});
--- server.js
+++ server.js
@@ -1,72 +1,121 @@
 var express = require("express");
-var mongojs = require("mongojs");
-var request = require("request");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
 var app = express();
 
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-var db = mongojs(databaseUrl, collections);
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
 
-app.get("/", function(req, res) {
-  res.render("index");
-});
+// Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/nprScraper");
 
-app.get("/api/all", function(req, res) {
-  db.scrapedData.find({}, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/api/scrape", function(req, res) {
-  request("https://www.nytimes.com/", function(error, response, html) {
-    var $ = cheerio.load(html);
-
-    $("article.story.theme-summary").each(function(i, element) {
-    //   var summary = $(element)
-    //     .children("p.summary")
-    //     .text();
-      var title = $(element)
-        .children("h2.story-heading")
-        .children("a")
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.npr.org/sections/news/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    var result = {};
+    $("div.item-info").each(function(i, element) {
+      result.title = $(this)
+        .find("h2.title")
         .text();
-      var link = $(element)
-        .children("h2.story-heading")
-        .children("a")
+      result.summary = $(this)
+        .find("p.teaser")
+        .find("a")
+        .text();
+      result.link = $(this)
+        .find("p.teaser")
+        .find("a")
         .attr("href");
 
-      db.scrapedData.save(
-        {
-          title: title,
-          link: link
-        },
-        function(err, saved) {
-          if (error) {
-            console.log(err);
-          } else {
-            console.log(saved);
-          }
-        }
-      );
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          return res.json(err);
+        });
     });
+    res.send("Scrape Complete");
   });
-  res.send("scarpe commplete");
 });
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+
+app.get("/api/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/api/articles/:id", function(req, res) {
+  db.Article.findById({ _id: req.params.id })
+    .populate("comment")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/api/articles/:id", function(req, res) {
+  db.Comments.create(req.body)
+    .then(function(dbComments) {
+      // return db.Article.findOneAndUpdate({},{ comment: dbComments._id },{ new: true });
+      return db.Article.findOneAndUpdate(
+        {},
+        { $push: { comment: dbComments._id } },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.delete("/api/articles/:id", function(req, res) {
+  db.Comments.findByIdAndRemove({ _id: req.params.id })
+    .then(function(err) {
+      if(err) throw err;
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
